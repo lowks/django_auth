fb8f960a9d18f507dd22eb5b1ab95b7740110b92
Lee Brooks <lee@kagisio.io>
2015-04-17 16:48:19 +0200

    Added CI specific settings

---
+++ kagiso_auth/tests/settings/ci.py
@@ -0,0 +1,13 @@
+from .test import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': 5434,
+        'ATOMIC_REQUESTS': True,
+    }
+}
