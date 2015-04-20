4219537c3678e265ebe138286528733f4a8bf0f8
Lee Brooks <lee@kagisio.io>
2015-04-20 14:28:20 +0200

    Initial model

--- auth_backend/models.py
+++ auth_backend/models.py
@@ -1,1 +1,34 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+# TODO: possible fields to add to CAS
+# first_name, last_name, is_staff, is_superadmin
+
+
+class KagisoUser(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = 'email'
+
+    email = models.EmailField(max_length=250, unique=True)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_staff(self):
+        # TODO: Implement
+        return False
+
+    @property
+    def is_superadmin(self):
+        # TODO: implement
+        return False
+
+    def get_full_name(self):
+        return self.email
+
+    def get_shortname(self):
+        return self.email
+
+    def set_password(self, raw_password):
+        # We don't want to save passwords locally
+        self.set_unusable_password()
+        # TODO: Update password on CAS?
--- auth_backend/tests/settings/test.py
+++ auth_backend/tests/settings/test.py
@@ -57,3 +57,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'auth_backend.KagisoUser'
