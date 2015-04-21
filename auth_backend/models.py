46720ec2a90e6792e0889409f8df24884ed20540
Lee Brooks <lee@kagisio.io>
2015-04-21 15:58:08 +0200

    WIP

--- auth_backend/models.py
+++ auth_backend/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from jsonfield import JSONField
 
+from . import auth_api_client
 from .managers import AuthManager
 
 # TODO: possible fields to add to CAS
@@ -12,8 +14,12 @@ class KagisoUser(AbstractBaseUser, PermissionsMixin):
 
     id = models.IntegerField(primary_key=True)
     email = models.EmailField(max_length=250, unique=True)
+    profile = JSONField(null=True)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+
+    confirmation_token = None
+    raw_password = None
 
     objects = AuthManager()
 
@@ -36,4 +42,37 @@ class KagisoUser(AbstractBaseUser, PermissionsMixin):
     def set_password(self, raw_password):
         # We don't want to save passwords locally
         self.set_unusable_password()
+        self.raw_password = raw_password
         # TODO: Update password on CAS?
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            self._create_for_db_and_cas()
+        else:
+            self.update_cas_user()
+
+        super().save()
+
+    def _create_for_db_and_cas(self):
+        payload = {
+            'email': self.email,
+            'password': self.raw_password,
+            'profile': self.profile,
+        }
+
+        status, data = auth_api_client.call('users', 'POST', payload)
+
+        if status == 409:
+            pass
+            # TODO: throw conflict error
+
+        if status != 201:
+            pass
+            # TODO: throw error
+
+        self.id = data['id']
+        self.confirmation_token = data['confirmation_token']
+        self.date_joined = data['date_joined']
+
+    def _update_cas_user(self):
+        pass
--- auth_backend/tests/test_models.py
+++ auth_backend/tests/test_models.py
@@ -1,7 +1,61 @@
+import json
+
+import responses
 from django.test import TestCase
+from model_mommy import mommy
 
 
-class SampleTest(TestCase):
+from .. import models
 
-    def test_sample(self):
-        assert True
+
+class KagisoUserTest(TestCase):
+
+    @responses.activate
+    def test_create_user_creates_centrally(self):
+        # ------------------------
+        # ##------Arrange------###
+        # ------------------------
+
+        url = 'https://auth.kagiso.io/api/v1/users/.json',
+        email = 'test@email.com'
+        profile = {
+            'is_superadmin': True
+        }
+
+        data = {
+            'id': 49,
+            'email': email,
+            'confirmation_token': '49:1YkTO2:1VuxvGJre66xqQj6rkEXewmVs08',
+            'email_confirmed': None,
+            'profile': profile,
+            'demographics': None,
+            'created': '2015-04-21T08:18:30.368602Z',
+            'modified': '2015-04-21T08:18:30.374410Z'
+        }
+
+        responses.add(
+            responses.POST,
+            url,
+            body=json.dumps(data),
+            status=201,
+        )
+
+        # ------------------------
+        # ##------Act------###
+        # ------------------------
+
+        user = mommy.make(models.KagisoUser, email=email, profile=profile)
+
+        # ------------------------
+        # ##------Assert------###
+        # ------------------------
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == url
+
+        assert user.id == data['id']
+        assert user.email == data['email']
+        assert user.confirmation_token == data['confirmation_token']
+        assert user.profile == data['profile']
+        assert user.demographics == data['demographics']
+        assert user.date_joined == data['created']
