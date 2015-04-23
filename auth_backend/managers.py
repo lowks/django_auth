9686f7c20abf9727d3cb8ff8dcace99560853c31
Lee Brooks <lee@kagisio.io>
2015-04-23 16:38:40 +0200

    Custom manager for creating users and superusers and tests

--- auth_backend/managers.py
+++ auth_backend/managers.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import BaseUserManager
 
+from .models import KagisoUser
+
 
 class AuthManager(BaseUserManager):
 
-    def create_user(self, email, password=None):
-        pass
+    def create_user(self, email, password=None, **other_fields):
+        user = KagisoUser(email=email, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-    def create_superuser(self, email, password):
-        pass
+    def create_superuser(self, email, password, **other_fields):
+        return self.create_user(email, password, is_superuser=True)
