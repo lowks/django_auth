f1d3ccc45988fcaa6bf1ea6523045c04245f18fb
Lee Brooks <lee@kagisio.io>
2015-05-05 13:41:54 +0200

    Removed some unnecessary print and TODOs

--- auth_backend/models.py
+++ auth_backend/models.py
@@ -77,19 +77,11 @@ class KagisoUser(AbstractBaseUser, PermissionsMixin):
         return status == 200
 
     def _create_user_in_db_and_cas(self):
-        # TODO: Look into calling methods when using mommy.make
-        # Support model mommy: need to be able to set password at time of
-        # creation ...
-        # if self.password:
-            # self.set_password(self.password)
-
         payload = {
             'email': self.email,
             'password': self.raw_password,
             'profile': self.profile,
         }
-
-        print(payload)
 
         status, data = auth_api_client.call('users', 'POST', payload)
 
