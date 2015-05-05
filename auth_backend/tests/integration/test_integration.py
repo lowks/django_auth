105c16c13b470de4a71a6bc5c633639b9ada5b6e
Lee Brooks <lee@kagisio.io>
2015-05-05 11:54:25 +0200

    Integration test for - creating users - confirming email addresses of new user - updating users - deleting users

--- auth_backend/tests/integration/test_integration.py
+++ auth_backend/tests/integration/test_integration.py
@@ -1,8 +1,51 @@
-# import responses
+from django.test import TestCase
+from model_mommy import mommy
 
-# responses.activate = lambda f: f
-# responses.deactivated = True
+from . import utils
+from ... import models
 
-# from ..unit.test_backends import *  # noqa
-# from ..unit.test_managers import *  # noqa
-# from ..unit.test_models import *  # noqa
+
+class KagisoUserTest(TestCase):
+
+    def test_user(self):
+        # ----- Create user -----
+        email = utils.random_email()
+        profile = {
+            'first_name': 'Fred',
+        }
+
+        user = mommy.make(
+            models.KagisoUser,
+            id=None,
+            email=email,
+            profile=profile,
+        )
+
+        result = models.KagisoUser.objects.get(id=user.id)
+
+        assert result.email == email
+        assert not result.confirmation_token
+        assert result.profile == profile
+
+        # ----- Confirm user -----
+        assert not user.email_confirmed
+        user.confirm_email(user.confirmation_token)
+        assert user.email_confirmed
+
+        # ----- Update user -----
+        new_email = utils.random_email()
+        new_profile = {
+            'first_name': 'George',
+        }
+
+        user.email = new_email
+        user.profile = new_profile
+        user.save()
+
+        updated_result = models.KagisoUser.objects.get(id=user.id)
+
+        assert updated_result.email == new_email
+        assert updated_result.profile == new_profile
+
+        # ----- Delete user -----
+        updated_result.delete()
