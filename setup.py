6ec2121a86dd5587f5bb48f552f3a0035743357c
Lee Brooks <lee@kagisio.io>
2015-05-14 15:28:49 +0200

    Added some extra fields in setup.py for Pypi's metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
     name='kagiso_django_auth',
     version='0.1',
+    author='Kagiso Media',
+    author_email='development@kagiso.io',
     url='https://github.com/Kagiso-Future-Media/django_auth',
+    packages=find_packages(),
     install_requires=[
             'jsonfield==1.0.3',
             'requests==2.6.0',
