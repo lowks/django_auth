f1eec2a7ceea1e2ae5223957f7ba1c52b6a8e069
Lee Brooks <lee@kagiso.io>
2015-06-25 13:55:46 +0200

    Have to hack around how Django calls a custom authentication backend

--- auth_backend/backends.py
+++ auth_backend/backends.py
@@ -6,7 +6,17 @@ from .models import KagisoUser
 
 class KagisoBackend(ModelBackend):
 
-    def authenticate(self, email, password, **kwargs):
+    def authenticate(self, email, password, username=None, **kwargs):
+        # Django calls our backend with username='xyz', password='abc'
+        # e.g. credentials = {'username': 'Fred', 'password': 'open'}
+        # authenticate(**credentials), even though we set USERNAME_FIELD to
+        # 'email' in models.py.
+        # So we have to hack around it:
+        # https://github.com/django/django/blob/master/django/contrib/auth/__init__.py#L74
+
+        if username:
+            email = username
+
         user = KagisoUser.objects.filter(email=email).first()
 
         if not user:
