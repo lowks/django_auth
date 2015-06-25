d2580ff27ebe01259f9591f8ac877c874ff48e7c
Lee Brooks <lee@kagiso.io>
2015-06-25 16:36:37 +0200

    Updated minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_django_auth',
-    version='0.1.2',
+    version='0.2.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     url='https://github.com/Kagiso-Future-Media/django_auth',
